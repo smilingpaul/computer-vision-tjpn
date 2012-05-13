69b1f28a9e95d6d77ab4687d27084f65bb7a2429
tim.jagla@gmail.com <tim.jagla@gmail.com@2c5e73cb-a313-e28d-7e65-65d57f9a993a>
2012-05-13 16:47:17 +0000

    - completely new solution

---
+++ trunk/signrecognition/swift/smainwindow.h
@@ -0,0 +1,33 @@
+#ifndef SMAINWINDOW_H
+#define SMAINWINDOW_H
+
+#include <QtGui/QMainWindow>
+
+class QAction;
+class QMenu;
+
+class SMainWindow : public QMainWindow
+{
+	Q_OBJECT;
+
+public:
+	SMainWindow(QWidget *parent = 0, Qt::WFlags flags = 0);
+	~SMainWindow();
+
+private slots:
+    void about();
+
+private:
+	void createActions();
+	void createMenus();
+	void createToolBars();
+	void createStatusBar();
+
+	QMenu *fileMenu;
+	QAction *exitAct;
+
+	QMenu *helpMenu;
+	QAction *aboutAct;
+};
+
+#endif // SMAINWINDOW_H
