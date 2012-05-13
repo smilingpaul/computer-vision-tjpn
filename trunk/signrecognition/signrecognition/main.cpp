71a6a3f66c0c47f32d068eb90d3819d524566eb2
tim.jagla@gmail.com <tim.jagla@gmail.com@2c5e73cb-a313-e28d-7e65-65d57f9a993a>
2012-05-13 00:39:11 +0000

    - not really productive but at least some ui basics - some reference papers with award winning traffic sign detection algorithms added (for dem lulz)

--- trunk/signrecognition/signrecognition/main.cpp
+++ trunk/signrecognition/signrecognition/main.cpp
@@ -5,7 +5,22 @@
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+
+#ifdef _DEBUG
+	a.setApplicationName("SIFT Sign Recognition - DEBUG");
+#else // _RELEASE
+	a.setApplicationName("SIFT Sign Recognition");
+#endif
+
+	a.setApplicationVersion("0.0.1");
+
 	signrecognition w;
+
+#ifdef _DEBUG
 	w.show();
+#else // _RELEASE
+	w.showMaximized();
+#endif
+
 	return a.exec();
 }
