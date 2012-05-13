7a476edf75c2193b2795374e810a9f0d691235a6
tim.jagla@gmail.com <tim.jagla@gmail.com@2c5e73cb-a313-e28d-7e65-65d57f9a993a>
2012-05-13 11:37:11 +0000

    - fixed bad naming - declarative names!!!

--- signrecognition/signrecognition/main.cpp
+++ signrecognition/signrecognition/main.cpp
@@ -1,26 +1,27 @@
+/// \file main.cpp
+///
+/// Entry Point of Swift - Traffic Sign Recognition Software based on a Scale-Invariant-Feature-Transform Algorithm.
+
 #include "stdafx.h"
-#include "signrecognition.h"
+#include "smainwindow.h"
 #include <QtGui/QApplication>
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
+	QApplication swift(argc, argv);
+	SMainWindow window;
+
+	swift.setApplicationVersion("0.0.1");
 
 #ifdef _DEBUG
-	a.setApplicationName("SIFT Sign Recognition - DEBUG");
+	swift.setApplicationName("Swift - DEBUG");
+	window.setWindowTitle("Swift - DEBUG");
+	window.show();
 #else // _RELEASE
-	a.setApplicationName("SIFT Sign Recognition");
+	swift.setApplicationName("Swift");
+	window.setWindowTitle("Swift");
+	window.showMaximized();
 #endif
 
-	a.setApplicationVersion("0.0.1");
-
-	signrecognition w;
-
-#ifdef _DEBUG
-	w.show();
-#else // _RELEASE
-	w.showMaximized();
-#endif
-
-	return a.exec();
+	return swift.exec();
 }
---
+++ signrecognition/signrecognition/sopencvwidget.h
@@ -0,0 +1,18 @@
+#ifndef SOPENCVWIDGET_H
+#define SOPENCVWIDGET_H
+
+#include <QGLWidget>
+
+class SOpenCVWidget : public QGLWidget
+{
+	Q_OBJECT
+
+public:
+	SOpenCVWidget(QWidget *parent);
+	~SOpenCVWidget();
+
+private:
+	
+};
+
+#endif // SOPENCVWIDGET_H
