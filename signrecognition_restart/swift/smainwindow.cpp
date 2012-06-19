47ce6f4f30621269f424e851f5d7130f629a6adf
tim.jagla@gmail.com <tim.jagla@gmail.com@2c5e73cb-a313-e28d-7e65-65d57f9a993a>
2012-06-19 10:09:13 +0000

    - initial commit

--- signrecognition_restart/swift/smainwindow.cpp
+++ signrecognition_restart/swift/smainwindow.cpp
@@ -8,30 +8,35 @@
 SMainWindow::SMainWindow(QWidget *parent, Qt::WFlags flags)
 	: QMainWindow(parent, flags)
 {
-	/// Call for UI Designer generated content.
-	/// At the moment this Project doesn't use the UI Designer because it is easier to code without.
-	//Ui.setupUi(this);
+	initialize();
 
+	/// Creates QActions which represent specific user commands
 	createActions();
 
-
-	/// Header
+	/// Creates the Menu Bar (File, Edit, Help, ...)
 	createMenus();
-	
+
 	/// Outer Interface
 	createToolBars();
 
 	/// Inner Interface
-	createDockWidgets();
+	createWidgets();
 
-
-	/// \todo implementing the opencv data viewer for qt
-	/// Main Window - Central Widget
-	//opencvWidget = new SOpenCVWidget();
-	//setCentralWidget(opencvWidget);
-
-	/// Footer
+	/// Creates the Status Bar at the bottom of the Window
 	createStatusBar();
+}
+
+void SMainWindow::initialize()
+{
+	/// QStringList of file format filters.
+	/// Ordered by relevance. Only file formats supported by the OpenCV library.
+	mFormatFilters << tr("All Files (*.*)")
+		<< tr("JPEG Files (*.jpeg *.jpg *.jpe)")
+		<< tr("Portable Network Graphics (*.png)")
+		<< tr("TIFF Files (*.tiff *.tif)")
+		<< tr("Windows Bitmaps (*.bmp *.dib)")
+		<< tr("Portable Image Format (*.pbm *.pgm *.ppm)")
+		<< tr("Sun Rasters (*.sr *.ras)");
 }
 
 /// Destructor of the main window
@@ -77,11 +82,14 @@ void SMainWindow::createMenus()
 	/// "File" Menu
 	fileMenu = menuBar()->addMenu(tr("&File"));
 	fileMenu->addAction(openAct);
-	fileMenu->addSeparator();
 	fileMenu->addAction(exitAct);
 
 	/// "Edit" Menu
 	editMenu = menuBar()->addMenu(tr("&Edit"));
+
+	/// "View" Menu
+	viewMenu = menuBar()->addMenu(tr("&View"));
+	viewMenu->addSeparator();
 
 	/// "Help" Menu
 	helpMenu = menuBar()->addMenu(tr("&Help"));
@@ -105,8 +113,32 @@ void SMainWindow::createToolBars()
 ///
 /// \return Nothing
 /// \sa 
-void SMainWindow::createDockWidgets()
+void SMainWindow::createWidgets()
 {
+	//////////////////////////////////////////////////////////////////////////
+	/// Dock Widget Alpha
+	dockAlpha = new QDockWidget(tr("Dock Alpha"),this);
+	dockAlpha->setObjectName("dockAlpha");
+	dockAlpha->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
+	dockAlpha->setFixedWidth(262); ///< 128px + 6px + 128px
+
+	dockContentsAlpha = new QWidget();
+	dockContentsAlpha->setObjectName("dockContentsAlpha");
+	dockAlpha->setWidget(dockContentsAlpha);
+	this->addDockWidget(Qt::LeftDockWidgetArea, dockAlpha);
+	viewMenu->addAction(dockAlpha->toggleViewAction());
+
+	//////////////////////////////////////////////////////////////////////////
+	/// Dock Widget Beta
+	dockBeta = new QDockWidget(tr("Dock Beta"),this);
+	dockBeta->setObjectName("dockBeta");
+	dockBeta->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
+
+	dockContentsBeta = new QWidget();
+	dockContentsBeta->setObjectName("dockContentsBeta");
+	dockBeta->setWidget(dockContentsBeta);
+	this->addDockWidget(Qt::RightDockWidgetArea, dockBeta);
+	viewMenu->addAction(dockBeta->toggleViewAction());
 
 }
 
@@ -121,31 +153,54 @@ void SMainWindow::createStatusBar()
 	statusBar()->showMessage(tr("Ready"));
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/// \brief
+///
+///
+///
+/// \return Nothing
+/// \sa
 void SMainWindow::open()
 {
-	QMessageBox::about(this, tr("OPEN DIALOG"),
-		tr("OPEN DIALOG PLACEHOLDER TEXT"));
+	 /// file open dialog to import images into the sign recognition software.
+	 /// it supports a multitude of file formats (see "filters").
+	mNewImagePaths = QFileDialog::getOpenFileNames(
+		this,
+		tr("Open File(s)"),
+		QDir::currentPath().append("/TestData"),
+		mFormatFilters.join(";;"),
+		&mFormatFilters[0],
+		nullptr
+		);
+
+	/// wrong user input might cause duplicate file paths in the "input"
+	/// string list. not exactly sure but we better be safe. we got the
+	/// processing power :-D
+	mNewImagePaths.removeDuplicates();
+
+	/// remove redundant paths from the new "input" string list.
+	/// redundant are paths which are already elements of the "paths"
+	/// string list.
+	for ( QStringList::Iterator it = mNewImagePaths.begin(); it != mNewImagePaths.end(); ++it )
+	{
+		if (mAllImagePaths.contains(*it)) ///< pointer-style iterator dereference
+		{
+			it = mNewImagePaths.erase(it);
+		}
+	}
+
+
+	//////////////////////////////////////////////////////////////////////////
+	// LOADING OF THE FILES
+
+	mAllImagePaths += mNewImagePaths; ///< add the new input to the "paths" member
 }
 
-
+/// \brief
+///
+///
+///
+/// \return Nothing
+/// \sa
 void SMainWindow::about()
 {
 	QMessageBox::about(this, tr("About Swift"),
