d451daad081d542a84611a889529622fe168e7a8
tim.jagla@gmail.com <tim.jagla@gmail.com@2c5e73cb-a313-e28d-7e65-65d57f9a993a>
2012-07-02 12:47:25 +0000

    - newest updates

--- signrecognition_restart/swift/imageitemexplore.cpp
+++ signrecognition_restart/swift/imageitemexplore.cpp
@@ -14,20 +14,38 @@ void ImageItemExplore::match(cv::DescriptorMatcher &matcher)
 	matcher.match(mDescriptors, mMatches);
 
 	mMatchesByIndex.clear();
+	mGoodMatchesByIndex.clear();
 
 	for (unsigned int i = 0; i < matcher.getTrainDescriptors().size(); i++)
+	{
 		mMatchesByIndex.push_back(std::vector<cv::DMatch>());
+		mGoodMatchesByIndex.push_back(std::vector<cv::DMatch>());
+	}
 
 	for (unsigned int i = 0; i < mMatches.size(); i++)
 	{
 		mMatchesByIndex[mMatches[i].imgIdx].push_back(mMatches[i]);
 	}
 
-	//matcher.knnMatch(mDescriptors,mMatchesByIndex,4);
+	matcher.knnMatch(mDescriptors,mMatchesByIndex,1);
 	//matcher.radiusMatch(mDescriptors,mMatchesByIndex,200);
+
+	// iterate through the vector collection
+	for (unsigned int i = 0; i < mMatchesByIndex.size(); i++)
+	{
+		// iterate through one vector
+		for (unsigned int j = 0; j < mMatchesByIndex[i].size(); j++)
+		{
+			if (mMatchesByIndex[i][j].distance < 200)
+				mGoodMatchesByIndex[i].push_back(mMatchesByIndex[i][j]);
+		}
+	}
+
+
 }
 
 //////////////////////////////////////////////////////////////////////////
 
 std::vector<cv::DMatch> ImageItemExplore::getMatches() { return mMatches; }
 std::vector<cv::DMatch> ImageItemExplore::getMatchesByIndex(int index) { return mMatchesByIndex[index]; }
+std::vector<cv::DMatch> ImageItemExplore::getGoodMatchesByIndex(int index) { return mGoodMatchesByIndex[index]; }
